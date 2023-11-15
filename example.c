1e5fe8fcc7a716180d2a323eeb66b6a7ea325a1b
Chelo <67635210+mariav7@users.noreply.github.com>
2023-11-15 08:30:53 +0100

    fix: indentation example.c

--- example.c
+++ example.c
@@ -4,16 +4,16 @@
 
 int main(void)
 {
-    char    c;
-    char    *str;
-    int     fd;
-    int     i;
+	char    c;
+	char    *str;
+	int     fd;
+	int     i;
 	char    *line;
-
-    c = 'S';
-    ft_printf("%d\n", ft_isalpha(c)); // libft.h
-    str = "Hello world! :)";
-    ft_printf("%s\n", str); //ft_printf.h
+	
+	c = 'S';
+	ft_printf("%d\n", ft_isalpha(c)); // libft.h
+	str = "Hello world! :)";
+	ft_printf("%s\n", str); //ft_printf.h
 
 	fd = open("example.c", O_RDONLY);
 	if (fd == -1)
@@ -23,10 +23,10 @@ int main(void)
 	while (line != NULL)
 	{
 		ft_printf("line [%d] : %s", ++i, line);
-        free(line);
+		free(line);
 		line = get_next_line(fd);
 	}
 	close(fd);
-
+	
 	return (0);
 }
